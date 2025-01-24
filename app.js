74a0788afcb44beeb71b3f84b927724d437f7844
rnslepw <rnslepw@gmail.com>
2025-01-24 21:39:51 +0300

    Add routes and pages

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages/index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages/about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages/contact.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages/404.html"));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
